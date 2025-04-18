f5d519acade75e8b7543b3c62be074cc41709fbd
ADM <aaacoadm@gmail.com>
2025-04-18 16:19:14 +0200

    Implementazione sistema di interruzioni e aggiornamento Makefile

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,7 +1,14 @@
+#include "interrupts.h"
+
 void main() {
+    // Inizializza il sistema di interruzioni
+    idt_init();
+    timer_init();
+    keyboard_init();
+
     // Video memory in modalità testo (0xb8000 è l'indirizzo per il video memory)
     char *video_memory = (char*) 0xB8000;
-
+    
     // Scriviamo il carattere 'A' e impostiamo il colore del testo (bianco su nero)
     video_memory[0] = 'A';
     video_memory[1] = 0x07;  // 0x07 è il colore bianco su sfondo nero
@@ -9,4 +16,9 @@ void main() {
     // Il prossimo carattere va alla posizione successiva
     // video_memory[2] = 'B'; 
     // video_memory[3] = 0x07; // E così via...
+
+    // Loop infinito per mantenere il kernel in esecuzione
+    while(1) {
+        asm volatile("hlt");  // Ferma la CPU fino all'interrotto successivo
+    }
 }
--- makefile
+++ makefile
@@ -1,15 +1,23 @@
 BOOTLOADER=bootloader/bootloader.asm
+KERNEL_OBJ=kernel/src/kernel.o kernel/src/interrupts.o kernel/src/timer.o kernel/src/keyboard.o
 BUILD_DIR=build
 OUTPUT=$(BUILD_DIR)/bootloader.bin
+KERNEL=$(BUILD_DIR)/kernel.bin
 
-all: $(OUTPUT)
+all: $(OUTPUT) $(KERNEL)
 
 $(OUTPUT): $(BOOTLOADER)
 	mkdir $(BUILD_DIR) 2> NUL || exit 0
 	nasm -f bin $(BOOTLOADER) -o $(OUTPUT)
 
+$(KERNEL): $(KERNEL_OBJ)
+	gcc -m32 -o $(KERNEL) $(KERNEL_OBJ)
+
+kernel/src/%.o: kernel/src/%.c
+	gcc -m32 -c $< -o $@
+
 run: all
-	qemu-system-i386 -fda $(OUTPUT)
+	qemu-system-i386 -fda $(OUTPUT) -hda $(KERNEL)
 
 clean:
 	del /Q $(BUILD_DIR)\*
